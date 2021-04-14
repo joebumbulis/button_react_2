a68d19d15c18dc71d081a81f15a77133a0509518
joebumbulis <joebumbulis@gmail.com>
2021-04-14 10:57:00 -0500

    updates test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,20 @@
 /* eslint-disable */
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, getByTestId, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders click me button', () => {
-  render(<App />);
-  const button = screen.getByText(/CLICK ME 💣/i);
-  expect(button).toBeInTheDocument();
-});
-
-test('calls onClick on button click', () => {
-  render(<App />);
-  expect(/Right Meow/).not.toBeInTheDocument;
-  const button = screen.getByText(/CLICK ME 💣/i);
-  fireEvent.click(button);
-  expect(screen.getByText(/Right Meow/i)).toBeInTheDocument;
-});
+describe('general rendering', () => {
+  it('renders click me button', () => {
+    render(<App />);
+    const button = screen.getByText(/CLICK ME 💣/i);
+    expect(button).toBeInTheDocument();
+  });
+  
+  it('calls onClick on button click', () => {
+    render(<App />);
+    expect(/Right Meow/).not.toBeInTheDocument;
+    const button = screen.getByText(/CLICK ME 💣/i);
+    fireEvent.click(button);
+    expect(screen.getByText(/Right Meow/i)).toBeInTheDocument;
+  });
+})
