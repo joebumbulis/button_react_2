1c3967f7b683338fc441e5f3b45e5977c43b4fc2
joebumbulis <joebumbulis@gmail.com>
2021-11-02 10:23:00 -0500

    makes updates to render button to me button

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,16 +4,16 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('general rendering', () => {
-  it('renders click button button', () => {
+  it('renders click me button', () => {
     render(<App />);
-    const button = screen.getByText(/CLICK BUTTON 💣/i);
+    const button = screen.getByText(/CLICK ME 💣/i);
     expect(button).toBeInTheDocument();
   });
   
   it('calls onClick on button click', () => {
     render(<App />);
     expect(/Right Meow/).not.toBeInTheDocument;
-    const button = screen.getByText(/CLICK BUTTON 💣/i);
+    const button = screen.getByText(/CLICK ME 💣/i);
     fireEvent.click(button);
     expect(screen.getByText(/Right Meow/i)).toBeInTheDocument;
   });
