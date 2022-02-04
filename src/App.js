ebc4829df581c5732b44fccca5df379d52988136
joebumbulis <joebumbulis@gmail.com>
2022-02-04 09:49:49 -0600

    adds extra button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import Button from './button';
 
 function App() {
   const [displayText, setDisplayText] = useState(false);
+  const [displaySecondText, setSecondDisplayText] = useState(false);
 
   const textToDisplay = <div className="display-text">Right Meow!💥😸</div>;
+  const secondText = <div className="display-text">Shablow!💥😜</div>;
+  const buttonTitle = 'CLICK BUTTON 💣';
+  const secondButtonTitle = 'CLICK ME TOO ☕️';
 
   return (
     <div className="App">
       <header className="App-header">
-        <Button onClick={() => setDisplayText(true)} />
+        <Button onClick={() => setDisplayText(true)} buttonTitle={buttonTitle} />
         {displayText && textToDisplay}
+
+        <p />
+
+        <Button onClick={() => setSecondDisplayText(true)} buttonTitle={secondButtonTitle} />
+        {displaySecondText && secondText}
       </header>
     </div>
   );
