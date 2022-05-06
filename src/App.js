5bb465d81f94f795fb10bb6cdfe08418a148edba
joebumbulis <joebumbulis@gmail.com>
2022-05-06 11:26:21 -0500

    creates failing test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const textToDisplay = <div className="display-text">Right Meow!💥😸</div>;
   const secondText = <div className="display-text">Shablow!💥😜</div>;
   const coffeeButtonTitle = 'CLICK ME TOO ☕️';
-  const buttonTitle = 'CLICK BUTTON 💣';
+  const buttonTitles = 'CLICK BUTTON 💣';
 
   return (
     <div className="App">
