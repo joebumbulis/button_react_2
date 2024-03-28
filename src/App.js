3d0eb18566e06a54254a094d58e76c600f33bf80
Joe Bumbulis <joebumbulis@gmail.com>
2024-03-28 11:27:57 -0500

    deletes code for preview

--- src/App.js
+++ src/App.js
@@ -5,17 +5,11 @@ import './styling/App.css';
 function App() {
   const [displayText, setDisplayText] = useState(false);
   const [displaySecondText, setCoffeeDisplayText] = useState(false);
-  const [displayLastText, setLastDisplayText] = useState(false);
-  const [displayUberText, setUberDisplayText] = useState(false);
 
   const textToDisplay = <div className="display-text">Right Meow!💥😸</div>;
   const secondText = <div className="display-text">Shablow!💥😜</div>;
-  const lastText = <div className="display-text">Kablow!💥😜</div>;
-  const uberText = <div className="display-text">Kablow!💥😜</div>;
   const coffeeButtonTitle = 'CLICK ME TOO ☕️';
   const bombButtonTitle = 'CLICK BUTTON 💣';
-  const lastButtonTitle = 'CLICKETY TADA';
-  const uberDisplayText = 'UBER CLICK';
 
   return (
     <div className="App">
@@ -29,11 +23,6 @@ function App() {
         {displaySecondText && secondText}
         <p />
 
-        <Button onClick={() => setLastDisplayText(true)} buttonTitle={lastButtonTitle} />
-        {displayLastText && lastText}
-
-        <Button onClick={() => setUberDisplayText(true)} buttonTitle={uberDisplayText} />
-        {displayUberText && uberText}
       </header>
     </div>
   );
